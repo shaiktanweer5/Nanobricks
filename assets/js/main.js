c1423c3daa1413012beda3545637d8a420e01d54
Tanweer Ahmed <38348535+shaiktanweer5@users.noreply.github.com>
2026-06-22 01:36:08 +0530

    Refactor main.js for improved navigation and back-to-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,56 @@
-(function() {
+(function () {
   "use strict";
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
 
-  // Back to top button
-  let backtotop = document.querySelector('.back-to-top');
-
-  if (backtotop) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active');
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener));
       } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  // Mobile Navigation Toggle
+
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+
+  // Close mobile menu after click
+
+  on('click', '#navbar .nav-link', function(e) {
+    let navbar = select('#navbar');
+    if (navbar.classList.contains('navbar-mobile')) {
+      navbar.classList.remove('navbar-mobile');
+      let navbarToggle = select('.mobile-nav-toggle');
+      navbarToggle.classList.toggle('bi-list');
+      navbarToggle.classList.toggle('bi-x');
+    }
+  }, true);
+
+  // Back To Top
+
+  let backtotop = select('.back-to-top');
+  if(backtotop){
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 100){
+        backtotop.classList.add('active');
+      }
+      else{
         backtotop.classList.remove('active');
       }
     });
   }
-
 })();
