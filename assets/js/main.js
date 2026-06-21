92879e521452905530ae30d2792ad89929765f28
Tanweer Ahmed <38348535+shaiktanweer5@users.noreply.github.com>
2026-06-22 01:44:25 +0530

    Refactor mobile navigation and back-to-top functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,68 @@
 (function () {
-  "use strict";
-  const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
-  };
+"use strict";
 
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener));
-      } else {
-        selectEl.addEventListener(type, listener);
-      }
-    }
-  };
+/* Back to top */
 
-  // Mobile Navigation Toggle
+let backtotop = document.querySelector('.back-to-top');
 
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile');
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
-  });
+if(backtotop){
 
-  // Close mobile menu after click
+window.addEventListener('scroll', () => {
 
-  on('click', '#navbar .nav-link', function(e) {
-    let navbar = select('#navbar');
-    if (navbar.classList.contains('navbar-mobile')) {
-      navbar.classList.remove('navbar-mobile');
-      let navbarToggle = select('.mobile-nav-toggle');
-      navbarToggle.classList.toggle('bi-list');
-      navbarToggle.classList.toggle('bi-x');
-    }
-  }, true);
+if(window.scrollY > 100){
 
-  // Back To Top
+backtotop.classList.add('active');
 
-  let backtotop = select('.back-to-top');
-  if(backtotop){
-    window.addEventListener('scroll', () => {
-      if(window.scrollY > 100){
-        backtotop.classList.add('active');
-      }
-      else{
-        backtotop.classList.remove('active');
-      }
-    });
-  }
+}else{
+
+backtotop.classList.remove('active');
+
+}
+
+});
+
+}
+
+
+/* Mobile Navigation */
+
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+
+if(mobileNavToggle){
+
+mobileNavToggle.addEventListener('click', function(){
+
+document.querySelector('#navbar').classList.toggle('navbar-mobile');
+
+this.classList.toggle('bi-list');
+
+this.classList.toggle('bi-x');
+
+});
+
+}
+
+
+/* Close menu after clicking */
+
+document.querySelectorAll('#navbar a').forEach(navlink => {
+
+navlink.addEventListener('click', () => {
+
+let navbar=document.querySelector('#navbar');
+
+if(navbar.classList.contains('navbar-mobile')){
+
+navbar.classList.remove('navbar-mobile');
+
+mobileNavToggle.classList.add('bi-list');
+
+mobileNavToggle.classList.remove('bi-x');
+
+}
+
+});
+
+});
+
 })();
