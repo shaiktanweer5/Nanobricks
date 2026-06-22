1b3eb93a4f94e4d835344bffca28b078ed1e7016
Tanweer Ahmed <38348535+shaiktanweer5@users.noreply.github.com>
2026-06-22 16:41:37 +0530

    Refactor main.js for mobile navigation and back to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,40 @@
 (function () {
-"use strict";
+  "use strict";
 
-/* Back to top */
+  const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+  const navbar = document.querySelector('#navbar');
 
-let backtotop = document.querySelector('.back-to-top');
+  if (mobileNavToggle) {
 
-if(backtotop){
+    mobileNavToggle.addEventListener('click', function () {
 
-window.addEventListener('scroll', () => {
+      navbar.classList.toggle('navbar-mobile');
 
-if(window.scrollY > 100){
+      this.classList.toggle('bi-list');
+      this.classList.toggle('bi-x');
 
-backtotop.classList.add('active');
+    });
 
-}else{
+  }
 
-backtotop.classList.remove('active');
+  let backtotop = document.querySelector('.back-to-top');
 
-}
+  if (backtotop) {
 
-});
+    window.addEventListener('scroll', () => {
 
-}
+      if (window.scrollY > 100) {
 
+        backtotop.classList.add('active');
 
-/* Mobile Navigation */
+      } else {
 
-const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+        backtotop.classList.remove('active');
 
-if(mobileNavToggle){
+      }
 
-mobileNavToggle.addEventListener('click', function(){
+    });
 
-document.querySelector('#navbar').classList.toggle('navbar-mobile');
-
-this.classList.toggle('bi-list');
-
-this.classList.toggle('bi-x');
-
-});
-
-}
-
-
-/* Close menu after clicking */
-
-document.querySelectorAll('#navbar a').forEach(navlink => {
-
-navlink.addEventListener('click', () => {
-
-let navbar=document.querySelector('#navbar');
-
-if(navbar.classList.contains('navbar-mobile')){
-
-navbar.classList.remove('navbar-mobile');
-
-mobileNavToggle.classList.add('bi-list');
-
-mobileNavToggle.classList.remove('bi-x');
-
-}
-
-});
-
-});
+  }
 
 })();
-window.addEventListener("load", function(){
-
-const loader=document.getElementById("loader");
-
-loader.style.opacity="0";
-
-setTimeout(()=>{
-
-loader.style.display="none";
-
-},500);
-
-});
